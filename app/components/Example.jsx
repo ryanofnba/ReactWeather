12c731429bb6d12512f4a63977bb6efdbc6ff5d4
Ryan Huang <ryan_huang@intuit.com>
2016-08-10 16:15:45 -0700

    added coloumns

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Example = (props) => {
   return (
-    <h3>Examples</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try</p>
+      <ol>
+        <li>
+          <Link to='/?location=Sacramento'>Sacramento, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
   );
 };
 
